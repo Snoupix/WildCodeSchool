9834f9307b9a4b46ad664d940d2b6227b6ced4f4
Snoupix <samufpsdu59@hotmail.fr>
2023-01-25 14:06:29 +0100

    Updated members list display

--- app/components/members_list.tsx
+++ app/components/members_list.tsx
@@ -11,8 +11,7 @@ export default function MembersList(props: {
 		<div className="mt-4 mb-16 flex flex-col justify-center w-full h-2/6">
 			<h2 className="text-xl text-center">{"Membres de l'équipage"}</h2>
 
-			<div className="mt-8 flex flex-col flex-wrap content-center justify-start items-center gap-5 h-44">
-				{/* <div className="grid grid-cols-3 gap-3" key={member.id}> */}
+			<div className="mt-8 grid justify-center three_cols_grid gap-5 h-44">
 				{props.data.map(member => (
 					<div className="flex flex-row items-center gap-3" key={member.id}>
 						<span className="">{member.name}</span>
